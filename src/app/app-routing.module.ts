a1bea961188c08590e822b6d3d1471a762c557a6
Jorge Vergara <javebratt@Jorges-iMac.local>
2020-02-16 09:42:17 -0500

    Adds the authentication guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,37 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './services/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
+    loadChildren: () =>
+      import('./home/home.module').then(m => m.HomePageModule),
+    canActivate: [AuthGuard]
   },
-  { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule) },
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./pages/login/login.module').then(m => m.LoginPageModule)
+  },
   {
     path: 'signup',
-    loadChildren: () => import('./pages/signup/signup.module').then(m => m.SignupPageModule)
+    loadChildren: () =>
+      import('./pages/signup/signup.module').then(m => m.SignupPageModule)
   },
   {
     path: 'reset-password',
-    loadChildren:
-      () => import('./pages/reset-password/reset-password.module').then(m => m.ResetPasswordPageModule)
+    loadChildren: () =>
+      import('./pages/reset-password/reset-password.module').then(
+        m => m.ResetPasswordPageModule
+      )
   },
   {
     path: 'profile',
-    loadChildren: () => import('./pages/profile/profile.module').then(m => m.ProfilePageModule)
+    loadChildren: () =>
+      import('./pages/profile/profile.module').then(m => m.ProfilePageModule),
+    canActivate: [AuthGuard]
   }
 ];
 
---
+++ src/app/services/auth.guard.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const user = await this.authService.getUser();
+        if (user) {
+          resolve(true);
+        } else {
+          reject('No user logged in');
+          this.router.navigateByUrl('/login');
+        }
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
+}
