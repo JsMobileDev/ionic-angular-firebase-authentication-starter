d3d1df444ed8c8b247122534ebf536e38222823f
Jorge Vergara <jorge@jorgevergara.co>
2022-04-05 16:00:45 -0500

    Updates profile service

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -1,62 +1,57 @@
 import { Injectable } from '@angular/core';
-import {
-  AngularFirestore,
-  AngularFirestoreDocument
-} from '@angular/fire/firestore';
 import { AuthService } from './auth.service';
-import { Observable } from 'rxjs';
-
-import firebase from 'firebase/app';
-import 'firebase/auth';
 import { UserProfile } from '../models/user';
+import { doc, DocumentData, DocumentReference, Firestore, getDoc, setDoc } from '@angular/fire/firestore';
+import { User, reauthenticateWithCredential, EmailAuthProvider, updateEmail, updatePassword } from '@angular/fire/auth';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProfileService {
-  private userProfile: AngularFirestoreDocument<UserProfile>;
-  private currentUser: firebase.User;
-  constructor(
-    private firestore: AngularFirestore,
-    private authService: AuthService
-  ) { }
+  private userProfileReference: DocumentReference<DocumentData>;
+  private currentUser: User;
+  constructor(private firestore: Firestore, private authService: AuthService) {}
 
-  async getUserProfile(): Promise<Observable<UserProfile>> {
-    const user: firebase.User = await this.authService.getUser();
+  async getUserProfileReference(): Promise<DocumentReference<DocumentData>> {
+    const user: User = await this.authService.getUser();
     this.currentUser = user;
-    this.userProfile = this.firestore.doc(`userProfile/${user.uid}`);
-    return this.userProfile.valueChanges();
+    return doc(this.firestore, `userProfile/${user.uid}`);
   }
 
-  updateName(fullName: string): Promise<void> {
-    return this.userProfile.update({ fullName });
+  async getUserProfile(): Promise<UserProfile | null> {
+    const userProfileReference = await this.getUserProfileReference();
+    const userProfile = await getDoc(userProfileReference);
+    return userProfile.exists() ? (userProfile.data() as UserProfile) : null;
+  }
+
+  async updateName(fullName: string): Promise<void> {
+    const userProfileReference = await this.getUserProfileReference();
+    return setDoc(userProfileReference, { fullName });
   }
 
   async updateEmail(newEmail: string, password: string): Promise<void> {
-    const credential: firebase.auth.AuthCredential = firebase.auth.EmailAuthProvider.credential(
-      this.currentUser.email,
-      password
-    );
+    const userProfile = await this.getUserProfile();
+    const credential = EmailAuthProvider.credential(userProfile.email, password);
+
     try {
-      await this.currentUser.reauthenticateWithCredential(credential);
-      await this.currentUser.updateEmail(newEmail);
-      return this.userProfile.update({ email: newEmail });
+      const user = await this.authService.getUser();
+      await reauthenticateWithCredential(user, credential);
+      await updateEmail(user, newEmail);
+      const userProfileReference = await this.getUserProfileReference();
+      return setDoc(userProfileReference, { email: newEmail });
     } catch (error) {
       console.error(error);
     }
   }
 
-  async updatePassword(
-    newPassword: string,
-    oldPassword: string
-  ): Promise<void> {
-    const credential: firebase.auth.AuthCredential = firebase.auth.EmailAuthProvider.credential(
-      this.currentUser.email,
-      oldPassword
-    );
+  async updatePassword(newPassword: string, oldPassword: string): Promise<void> {
+    const userProfile = await this.getUserProfile();
+    const credential = EmailAuthProvider.credential(userProfile.email, oldPassword);
+
     try {
-      await this.currentUser.reauthenticateWithCredential(credential);
-      return this.currentUser.updatePassword(newPassword);
+      const user = await this.authService.getUser();
+      await reauthenticateWithCredential(user, credential);
+      return await updatePassword(user, newPassword);
     } catch (error) {
       console.error(error);
     }
