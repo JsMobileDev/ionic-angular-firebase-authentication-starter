23858274b6fc7da5d1cf7e93816b3a36cf4ed8d7
Jorge Vergara <jorge@jorgevergara.co>
2022-04-05 16:03:33 -0500

    updates auth pages

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -3,25 +3,21 @@ import { UserCredential } from 'src/app/models/user';
 import { AuthService } from 'src/app/services/auth.service';
 import { AuthFormComponent } from 'src/app/components/auth-form/auth-form.component';
 import { Router } from '@angular/router';
-import firebase from 'firebase/app';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss']
+  styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
   @ViewChild(AuthFormComponent) loginForm: AuthFormComponent;
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(private authService: AuthService, private router: Router) {}
 
-  ngOnInit() { }
+  ngOnInit() {}
 
   async loginUser(credentials: UserCredential): Promise<void> {
     try {
-      const userCredential: firebase.auth.UserCredential = await this.authService.login(
-        credentials.email,
-        credentials.password
-      );
+      const userCredential = await this.authService.login(credentials.email, credentials.password);
       this.authService.userId = userCredential.user.uid;
       await this.loginForm.hideLoading();
       this.router.navigateByUrl('home');
